1ddf54398941929b4886127dd5a26882d8aa0163
Ducky <electricduck@outlook.com>
2020-10-01 22:45:17 +0000

    🆕 feat(Get-HelloOSDetails): improve performance of OS detection

--- Install-Hello.ps1
+++ Install-Hello.ps1
@@ -115,86 +115,69 @@ function Get-HelloOSDetails {
     $OSRelease = ""
     $OSVersion = [Environment]::OSVersion.Version
 
-    $LinuxOSRelease = $null
-    $WindowsCurrentVersion = $null
-
-    if ($OS -eq [OS]::Linux) {
-        if (Test-Path /etc/os-release) {
-            $LinuxOSRelease = (Get-Content /etc/os-release)
-        }
-    }
-
-    if ($OS -eq [OS]::Windows) {
-        $WindowsCurrentVersion = Get-ItemProperty -Path Registry::"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-    }
-
-    function Get-OSName {
-        switch ($OS) {
-            Linux {
-                if ($LinuxOSRelease) {
-                    (($LinuxOSRelease | select-string "NAME")[0].ToString().Replace("NAME=", "").Replace("`"", "")).
-                    Replace("elementary OS", "elementaryOS").
-                    Replace("Debian GNU/Linux", "Debian")
-                }
-                else {
-                    "Linux"
+    switch ($OS) {
+        Linux {
+            $LinuxOSRelease = if ($OS -eq [OS]::Linux) {
+                if (Test-Path /etc/os-release) {
+                    (Get-Content /etc/os-release)
                 }
             }
-            MacOS {
-                if ($OSVersion.Build -lt 16) {
-                    "OSX"
-                }
-                elseif ($OSVersion.Build -le 16) {
-                    "macOS"
-                }
+
+            if ($LinuxOSRelease) {
+                $OSName = (($LinuxOSRelease | select-string "NAME")[0].ToString().Replace("NAME=", "").Replace("`"", "")).
+                Replace("elementary OS", "elementaryOS").
+                Replace("Debian GNU/Linux", "Debian")
+
+                $OSRelease = ($LinuxOSRelease | select-string "VERSION")[0].ToString().Replace("VERSION=", "").Replace("`"", "")
             }
-            Windows {
-                if ($WindowsCurrentVersion.ProductName) {
-                    $WindowsCurrentVersion.ProductName.
-                    Replace("Microsoft Windows", "Windows")
-                }
-                else {
-                    "Windows"
-                }
+            else {
+                $OSName = "Linux"
+                $OSRelease = "$($OSVersion.Major).$($OSVersion.Minor).$($OSVersion.Build)"
             }
         }
-    }
+        MacOS {
+            if ($OSVersion.Build -lt 16) {
+                $OSName = "OSX"
+            }
+            elseif ($OSVersion.Build -le 16) {
+                $OSName = "macOS"
+            }
 
-    function Get-OSRelease {
-        switch ($OS) {
-            Linux {
-                if ($LinuxOSRelease) {
-                    ($LinuxOSRelease | select-string "VERSION")[0].ToString().Replace("VERSION=", "").Replace("`"", "")
-                }
-                else {
-                    "$($OSVersion.Major).$($OSVersion.Minor).$($OSVersion.Build)"
-                }
+            $OSRelease = "$($OSVersion.Major).$($OSVersion.Minor)"
+        }
+        Windows {
+            $WindowsCurrentVersion = if ($OS -eq [OS]::Windows) {
+                Get-ItemProperty -Path Registry::"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
             }
-            MacOS {
-                "$($OSVersion.Major).$($OSVersion.Minor)"
+
+            if ($WindowsCurrentVersion.ProductName) {
+                $OSName = $WindowsCurrentVersion.ProductName.
+                Replace("Microsoft Windows", "Windows")
             }
-            Windows {
-                if ($WindowsCurrentVersion.CSDVersion) {
-                    $WindowsCurrentVersion.CSDVersion
-                }
-                elseif ($WindowsCurrentVersion.ReleaseId) {
-                    $WindowsCurrentVersion.ReleaseId
-                }
-                elseif ($OSVersion.Build -ge 9841) {
-                    "Build $($OSVersion.Build)"
-                }
-                elseif (
-                    # TODO: Better way of picking up official builds that don't have a ReleaseId/CSDVersion
-                    $OSVersion.Build -eq 7600 -or # 7 / 2008 R2
-                    $OSVersion.Build -eq 9200 -or # 8 / 2012
-                    $OSVersion.Build -eq 9200 -or # 8.1 / 2012 R2
-                    $OSVersion.Build -eq 10240    # 10 1507
-                ) {
-                    ""
-                }
-                else {
-                    "$($OSVersion.Major).$($OSVersion.Minor).$($OSVersion.Build)"
-                }
+            else {
+                $OSName = "Windows"
+            }
+
+            if ($WindowsCurrentVersion.CSDVersion) {
+                $WindowsCurrentVersion.CSDVersion
+            }
+            elseif ($WindowsCurrentVersion.ReleaseId) {
+                $WindowsCurrentVersion.ReleaseId
+            }
+            elseif ($OSVersion.Build -ge 9841) {
+                "Build $($OSVersion.Build)"
+            }
+            elseif (
+                # TODO: Better way of picking up official builds that don't have a ReleaseId/CSDVersion
+                $OSVersion.Build -eq 7600 -or # 7 / 2008 R2
+                $OSVersion.Build -eq 9200 -or # 8 / 2012
+                $OSVersion.Build -eq 9600 -or # 8.1 / 2012 R2
+                $OSVersion.Build -eq 10240    # 10 1507
+            ) {
+                ""
+            }
+            else {
+                "$($OSVersion.Major).$($OSVersion.Minor).$($OSVersion.Build)"
             }
         }
     }
